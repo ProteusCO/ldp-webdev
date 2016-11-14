340ba89375d4744a04ed71f8a1892637ea0fd421
Russ Tennant <russ@venturetech.net>
2016-11-14 17:56:51 -0600

    Have the goal wizard navigation validate the goal wizard step.
    LR-1672

---
+++ web/src/javascript/templates/common-scripts/_goal-wizard.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    function getForm() {
+        var ret = null;
+        $('form.miwt-form').each(function(idx, form){
+            var el = form.elements['goal-wizard'];
+            if(el)
+                ret = form;
+        });
+        return ret;
+    }
+
+    $('.wizard-menu a.menuitemlabel').each(function(idx, anchor){
+        $(anchor).on('click', function(evt){
+            if(anchor._skip_gw) {
+                anchor._skip_gw = false;
+                return true;
+            }
+            var form = getForm();
+            if(!form) return;
+            var so = form.submit_options, origPostUpdate = so && so.postUpdate;
+            function goalWizardPostUpdate(data) {
+                var hasError = data.nodesUpdated.reduce(function(bval, node) {
+                    return bval || ($(node).find('.error.message').length > 0);
+                }, false);
+
+                if(origPostUpdate) origPostUpdate.call(this, data);
+                if(!hasError) {
+                    anchor._skip_gw = true;
+                    form.elements['goal-wizard'].value = 'true';
+                    anchor.click();
+                }
+            }
+            var newOpts = { postUpdate: goalWizardPostUpdate};
+            if(so) newOpts = $.extend({}, so, newOpts);
+            form.elements['goal-wizard'].value = 'validate';
+            form.MIWTSubmit(newOpts);
+            return false;
+        });
+    });
+
+});
