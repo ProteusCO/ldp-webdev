93cb7d94041658eea5b4720c98e3b0af08d13bb3
Russ Tennant <russ@venturetech.net>
2016-11-18 14:12:57 -0600

    Support important messages

--- web/src/javascript/templates/common-scripts/_error-messages.js
+++ web/src/javascript/templates/common-scripts/_error-messages.js
@@ -2,6 +2,7 @@
  * Created by vtdev on 4/7/16.
  */
 var CN_HAS_ERROR = "has-error";
+var CN_HAS_IMPORTANT = "has-important";
 
 function errorMessageCleanup(){
 	$("." + CN_HAS_ERROR).each(function (idx, el) {
@@ -9,9 +10,22 @@ function errorMessageCleanup(){
 		$el.removeClass(CN_HAS_ERROR);
 		$el.find('.error-message').remove();
 	});
+	$("." + CN_HAS_IMPORTANT).each(function (idx, el) {
+		var $el = $(el);
+		$el.removeClass(CN_HAS_IMPORTANT);
+		$el.find('.important-message').remove();
+	});
 }
 
 function setupErrorMessages(ctx){
+	function createWrapper($prop){
+		var $it;
+		if($prop.hasClass('ctb') || $prop.hasClass('rtb'))
+			$it = $prop.find('label').wrap('<span></span>');
+		else
+			$it = $prop.wrap("<span></span>");
+		return $it;
+	}
 	var $ctx = $(ctx || document), hasErrors;
 	var $mcs = $ctx.hasClass('message-container') ? $ctx : $ctx.find('.message-container');
 	if($mcs.length === 0)
@@ -24,14 +38,29 @@ function setupErrorMessages(ctx){
 			if($prop.length === 0)
 				return;
 			if($prop.prop('tagName').toLowerCase() != 'div') {
-				$prop.wrap("<span></span>");
-				$prop.parent().addClass(CN_HAS_ERROR);
-				$prop.parent().append('<span class="error-message"><span class="error-text">' + $el.text() + '</span></span>');
+				var $it = createWrapper($prop);
+				$it.addClass(CN_HAS_ERROR);
+				$it.append('<span class="error-message"><span class="error-text">' + $el.text() + '</span></span>');
 				hasErrors = true;
 			} else {
 				$prop.addClass(CN_HAS_ERROR);
 				$prop.append('<div class="error-message"><span class="error-text">' + $el.text() + '</span></div>');
 				hasErrors = true;
+			}
+			$el.parent().remove();
+		});
+		$mc.find('.important [data-source]').each(function (idx, el) {
+			var $el = $(el), id = $el.data('source');
+			var $prop = $('#' + id);
+			if($prop.length === 0)
+				return;
+			if($prop.prop('tagName').toLowerCase() != 'div') {
+				var $it = createWrapper($prop);
+				$it.addClass(CN_HAS_IMPORTANT);
+				$it.append('<span class="important-message"><span class="important-text">' + $el.text() + '</span></span>');
+			} else {
+				$prop.addClass(CN_HAS_IMPORTANT);
+				$prop.append('<div class="important-message"><span class="important-text">' + $el.text() + '</span></div>');
 			}
 			$el.parent().remove();
 		});
