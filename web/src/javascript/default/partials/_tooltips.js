feece373ebbd9ae87f936fa22359d69328050144
Alan Holt <aholt@venturetech.net>
2018-01-24 15:58:56 -0600

    updated webdev for additional issues seen on QA with bootstrap 4 release

--- web/src/javascript/default/partials/_tooltips.js
+++ web/src/javascript/default/partials/_tooltips.js
@@ -15,8 +15,16 @@ function enableTooltips(ctx) {
 		});
 		$ctx.find('[data-toggle="tooltip"]').tooltip();
 	}
-	$ctx.find('[data-toggle="popover"]').popover().each(function (idx, el) {
+	$ctx.find('[data-toggle="popover"]').each(function (idx, el) {
 		var $el = $(el);
+		//Fix for API-braking changes to popovers between bootstrap 4 alpha and bootstrap 4 release
+		var template = $el.data('template');
+		if(typeof template !== 'undefined' && template) {
+			template = template.replace('popover-content', 'popover-body').replace('popover-arrow', 'arrow');
+			$el.data('template', template);
+		}
+		//End fix.
+		$el.popover();
 		$el.click(function () {
 			$el.popover('toggle');
 		});
