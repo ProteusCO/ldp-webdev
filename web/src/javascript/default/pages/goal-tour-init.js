26fb2d82ec4f188a15f9d42383ddb9fa19fdd98b
Alan Holt <aholt@venturetech.net>
2018-01-25 13:28:42 -0600

    fixed issues with the tour compatibility with bootstrap 4

--- web/src/javascript/default/pages/goal-tour-init.js
+++ web/src/javascript/default/pages/goal-tour-init.js
@@ -72,70 +72,82 @@ $(function () {
 				element: ".dropdown.profile",
 				title: "Your profile",
 				content: "Click the image to access your profile or logout of the platform.",
-				placement: "left"
+				placement: "left",
+                animation: false
 			},
 			{
 				element: ".top-menu li.link.resources",
 				title: "Resource Library",
 				content: "Click here to view resources available to all Accelerate members.",
-				placement: "left"
+				placement: "left",
+                animation: false
 			},
 			{
 				element: ".sidebar-toggle-box .fa-bars",
 				title: "Menu Toggle",
 				content: "Click here to collapse the side navigation to icons only, or expand it to view the entire navigation.",
-				placement: "right"
+				placement: "right",
+                animation: false
 			},
 			{
 				element: ".lr-participant-menu .goals",
 				title: "Goals",
-				content: "Click here to view Emerging Leader goals."
+				content: "Click here to view Emerging Leader goals.",
+                animation: false
 			},
 			{
 				element: ".lr-participant-menu .timeline-nav",
 				title: "Timeline",
-				content: "Click here to view Emerging Leader timeline - the development journey."
+				content: "Click here to view Emerging Leader timeline - the development journey.",
+                animation: false
 			},
 			{
 				element: ".lr-participant-menu .profile",
 				title: "Profile",
-				content: "Click here to view Emerging Leader profile."
+				content: "Click here to view Emerging Leader profile.",
+                animation: false
 			},
 			{
 				element: ".lr-participant-menu .resources",
 				title: "My Resources",
-				content: "Click here to view resources assigned to Emerging Leader timeline."
+				content: "Click here to view resources assigned to Emerging Leader timeline.",
+                animation: false
 			},
 			{
 				element: ".tab-container li.organizational",
 				title: "Organizational Tab",
 				content: "These goals directly impact the current organizational/business goals.",
-				placement: "right"
+				placement: "right",
+                animation: false
 			},
 			{
 				element: ".tab-container li.leadership",
 				title: "Leadership Tab",
 				content: "Goals related to leadership and/or behaviors and/or attitudes.",
-				placement: "right"
+				placement: "right",
+                animation: false
 			},
 			{
 				element: ".tab-container li.personal",
 				title: "Personal Tab",
 				content: "Goals outside of business, important to the individual, but not necessarily directly tied" +
 				" to EDM expectations.",
-				placement: "right"
+				placement: "right",
+                animation: false
 			},
 			{
 				element: ".goals-viewer > .persistence-actions button.completed-goals-toggle",
 				title: "Show Completed Goals",
 				content: "Click here to see goals that have already been completed.",
-				placement: "left"
+				placement: "left",
+                animation: false
 			},
 			{
 				element: ".goals-viewer > .persistence-actions button.configure",
 				title: "Configure Goals",
 				content: "Click here to add new goals or edit existing goals.",
-				placement: "left"
+				placement: "left",
+                animation: false
 			}/*,
 			{
 				// Automatically go to Timeline page
@@ -147,7 +159,19 @@ $(function () {
 		],
 		storage: false,
 		backdrop: true,
-		backdropPadding: 5
+		backdropPadding: 5,
+        template:
+        "<div class='popover tour' style='max-width: 300px;'>" +
+        	"<div class='arrow'></div>" +
+        	"<h3 class='popover-title popover-header'></h3>" +
+        	"<div class='popover-body popover-content'></div>" +
+        	"<div class='popover-navigation'>" +
+        		"<button class='btn btn-default' data-role='prev'>« Prev</button>" +
+        		"<span data-role='separator' style='margin-right: .5rem;'>|</span>" +
+        		"<button class='btn btn-default' data-role='next'>Next »</button>" +
+        		"<button class='btn btn-default' data-role='end'>End tour</button>" +
+        	"</div>" +
+        "</div>"
 	});
 
 	// Instance the EDM tour
@@ -158,58 +182,68 @@ $(function () {
 				element: ".sidebar-toggle-box .fa-bars",
 				title: "Menu Toggle",
 				content: "Click here to collapse the side navigation to icons only, or expand it to view the entire navigation.",
-				placement: "right"
+				placement: "right",
+                animation: false
 			},
 			{
 				element: ".lr-participant-menu .goals",
 				title: "Goals",
-				content: "Click here to view Emerging Leader goals."
+				content: "Click here to view Emerging Leader goals.",
+                animation: false
 			},
 			{
 				element: ".lr-participant-menu .timeline-nav",
 				title: "Timeline",
-				content: "Click here to view Emerging Leader timeline - the development journey."
+				content: "Click here to view Emerging Leader timeline - the development journey.",
+                animation: false
 			},
 			{
 				element: ".lr-participant-menu .profile",
 				title: "Profile",
-				content: "Click here to view Emerging Leader profile."
+				content: "Click here to view Emerging Leader profile.",
+                animation: false
 			},
 			{
 				element: ".lr-participant-menu .resources",
 				title: "My Resources",
-				content: "Click here to view resources assigned to Emerging Leader timeline."
+				content: "Click here to view resources assigned to Emerging Leader timeline.",
+                animation: false
 			},
 			{
 				element: ".tab-container li.organizational",
 				title: "Organizational Tab",
 				content: "These goals directly impact the current organizational/business goals.",
-				placement: "right"
+				placement: "right",
+                animation: false
 			},
 			{
 				element: ".tab-container li.leadership",
 				title: "Leadership Tab",
 				content: "Goals related to leadership and/or behaviors and/or attitudes.",
-				placement: "right"
+				placement: "right",
+                animation: false
 			},
 			{
 				element: ".tab-container li.personal",
 				title: "Personal Tab",
 				content: "Goals outside of business, important to the individual, but not necessarily directly tied" +
 				" to EDM expectations.",
-				placement: "right"
+				placement: "right",
+                animation: false
 			},
 			{
 				element: ".goals-viewer > .persistence-actions button.completed-goals-toggle",
 				title: "Show Completed Goals",
 				content: "Click here to see goals that have already been completed.",
-				placement: "left"
+				placement: "left",
+                animation: false
 			},
 			{
 				element: ".goals-viewer > .persistence-actions button.configure",
 				title: "Configure Goals",
 				content: "Click here to add new goals or edit existing goals.",
-				placement: "left"
+				placement: "left",
+                animation: false
 			},
 			{
 				// Automatically go to Timeline page
@@ -221,7 +255,19 @@ $(function () {
 		],
 		storage: false,
 		backdrop: true,
-		backdropPadding: 5
+		backdropPadding: 5,
+        template:
+        "<div class='popover tour' style='max-width: 300px;'>" +
+        	"<div class='arrow'></div>" +
+        	"<h3 class='popover-title popover-header'></h3>" +
+        	"<div class='popover-body popover-content'></div>" +
+        	"<div class='popover-navigation'>" +
+        		"<button class='btn btn-default' data-role='prev'>« Prev</button>" +
+        		"<span data-role='separator' style='margin-right: .5rem;'>|</span>" +
+        		"<button class='btn btn-default' data-role='next'>Next »</button>" +
+        		"<button class='btn btn-default' data-role='end'>End tour</button>" +
+        	"</div>" +
+        "</div>"
 	});
 
 	launchTourIfNecessary(participantGoalTour);
