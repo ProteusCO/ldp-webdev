dcb07d37213982421259807fdebe978080894d95
Alan Holt <aholt@venturetech.net>
2018-02-12 10:12:12 -0600

    fixed responsive css on development provider selector

--- web/src/javascript/default/pages/page--provider-selector.js
+++ web/src/javascript/default/pages/page--provider-selector.js
@@ -2,7 +2,7 @@ function setupBackgroundImages () {
 	$('.coaching-selector.standalone .coaching-item[data-background-image]').each(function (idx, target) {
 		var backgroundurl = target.getAttribute('data-background-image')
 		var styleString = ';overflow: hidden;'
-			+ 'text-indent: 0;'
+			+ 'text-indent: 80px;'
 			+ 'margin: .5rem 0;'
 			+ 'white-space: nowrap;'
 			+ 'width: 100%;'
@@ -13,7 +13,6 @@ function setupBackgroundImages () {
 			+ 'text-transform: none;'
 			+ 'padding: .5rem 1rem .5rem 1rem;'
 			+ 'background: url(' + backgroundurl + ') left center/contain no-repeat;'
-			+ 'text-align: center;'
 			+ 'background-position-x: 5%;'
 			+ 'cursor: pointer;'
 		target.style.cssText += styleString
